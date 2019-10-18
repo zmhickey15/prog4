ca321ab295b7613d2ce8d9959e4c420a94dbfc8c
zmhickey15 <56423312+zmhickey15@users.noreply.github.com>
2019-10-18 10:44:56 -0500

    Create prog4_mkh115_zmh11.cpp

---
+++ prog4_mkh115_zmh11.cpp
@@ -0,0 +1,71 @@
+/*
+   File: prog4_mkh115_zmh11.cpp  // Remove this inline comment after replacing 'bb0000' with your account number.
+
+   Author: Betty Boop      // Remove this inline comment after replacing Betty Boop with your first & last names.
+   C.S.1428.?              // Remove this inline comment after replacing the '?' with your three-digit lecture section number.
+   Lab Section: L?         // Remove this inline comment after replacing the '?' with your two-digit lab section number.
+   Program: #4
+   Due Date: --/--/--      // Remove this inline comment after replacing the dashes with the due date, in this format - month, day, year.
+
+   This program prompts the user for the number of items purchased. If no
+   items were purchased, the program terminates after printing an appropriate
+   message to the screen; otherwise, the program repeatedly prompts the
+   user for the price of the specified number of items and calculates the
+   total of all purchased items. It then calculates the shipping charge,
+   discounted total, tax on the discounted total, and the total amount due.
+
+   After all calculations are performed, the program prints the author's
+   identifying information followed by the total of all purchased items,
+   the discounted total, the tax rate, the tax on the discounted total,
+   shipping charges and the total amount due as shown below. Output is
+   written in receipt form to both the console and to an output file.
+
+   With the exception of the tax rate, the numeric data is formatted to
+   two decimal places. The tax rate is formatted to four decimal places.
+
+   Input (keyboard): number of items purchased, integer
+                     item price, double
+
+   Constants:        discount amount, double (sample: 0.20)
+                     cut off amount for free shipping, double
+                     shipping charge, double
+                     tax rate, double (sample: 0.0825)
+
+   Output (console):
+          Sample Console Output (These samples do not reflect prompts.)
+
+          Author's Name
+          C.S.1428.?        // '?' represents three-digit lecture section number
+          Lab Section: L?   // '?' represents two-digit lab section number
+          --/--/--          // dashes represent due date, month/day/year
+               <blank line>
+          Total Purchases:    $ ??.??
+          Discounted Total:     ??.??
+          Tax Rate:              ?.????
+          Tax:                   ?.??
+          Shipping:              ?.??
+               <blank line>
+          Total Amount Due:   $ ??.??
+               <blank line>
+               <blank line>
+          A copy for your records can be found in prog4_00?out.txt.
+
+   Output (file - prog4_?out.txt):
+
+          Author's Name
+          C.S.1428.?        // '?' represents three-digit lecture section number
+          Lab Section: L?   // '?' represents two-digit lab section number
+          --/--/--          // dashes represent due date, month/day/year
+               <blank line>
+          Total Purchases:    $ ??.??
+          Discounted Total:     ??.??
+          Tax Rate:              ?.????
+          Tax:                   ?.??
+          Shipping:              ?.??
+               <blank line>
+          Total Amount Due:   $ ??.??
+
+          ================================================
+
+          <Output will vary based on actual input values.>
+*/
